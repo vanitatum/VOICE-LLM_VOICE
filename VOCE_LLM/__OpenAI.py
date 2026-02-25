356dd3094a09c871e3baace9927944df0f1bc0ba
GYU0000 <132132708+0G0Y0U0@users.noreply.github.com>
2026-02-25 09:17:34 +0900

    코드 리팩토링 & 검토 기능 추가

--- VOCE_LLM/__OpenAI.py
+++ VOCE_LLM/__OpenAI.py
@@ -12,6 +12,10 @@ from scipy.io.wavfile import write #.wav 확장자 라이브러리
 
 import pyttsx3 #Text To Speech
 
+from tqdm import tqdm # 간지용 게이지 바
+
+import time # 대기 함수용 라이브러리
+
 
 
 sd.default.device = 1 # 녹음 장치
@@ -30,56 +34,76 @@ AfterFileLen = BeforeFileLen + 1
 Before_path = base_dir / "Log" / f"{BeforeFileLen}.txt"
 After_path = base_dir / "Log" / f"{AfterFileLen}.txt"
 
-if BeforeFileLen == 0: # 로그 파일이 아에 없을 경우.
-    ef = open(After_path, "w+", encoding="utf-8")
-    ef.write("과거 대화 기록이 없습니다.")
+if BeforeFileLen == 0: # 과거 로그 파일이 없을 경우.
+    with open(After_path, "w+", encoding="utf-8") as ef:
+        ef.write("과거 대화 기록이 없습니다.")
     Before_path = base_dir / "Log" / "1.txt"
-    ef.close()
 
 Setting_path = base_dir / "Setting.txt"
-Setting = open(Setting_path, "r", encoding="utf-8")
+with open(Setting_path, "r", encoding="utf-8") as Setting:
+    Setting = Setting.read()
 
 
-f = open(Before_path, "r", encoding="utf-8")
-output_base = "AI의 마지막 응답 (이것에 대해 먼저 언급하지 말 것.)", f.read()
+with open(Before_path, "r", encoding="utf-8") as f:
+    output_base = "AI의 마지막 응답 (이것에 대해 먼저 언급하지 말 것.)" + str(f.read())
 
 # Record
 samplerate = 16000 #샘플링 HZ
-duration = 2 # 녹음 시간(s)
-
 sound_path = base_dir / "voice.wav"
 
-print("녹음 시작")
-
-recording = sd.rec(int(samplerate * duration), samplerate = samplerate, channels=1, dtype='int16')
-
-sd.wait()
-
-print("녹음 끝")
-
-write(fr"{sound_path}", samplerate, recording)
-
-# STT
-
-model = None
+model = None # 모델을 1회만 부르기 위한 처리 구문. (STT) 구조상 이곳 위치
 if model is None:
     model = whisper.load_model("base")
-stt_result = model.transcribe(fr"{sound_path}")
-input_prompt = stt_result["text"]
 
-print("프롬포트 : ", input_prompt)
+def InputRecordStart():
+    VoiceSeconds = int(input("녹음 시간(초) 입력 : "))
+    if VoiceSeconds >= 1:
+        duration = VoiceSeconds # 녹음 시간(s)
+        print("녹음 시작")
+        recording = sd.rec(int(samplerate * duration), samplerate = samplerate, channels=1, dtype='int16')
+
+        tqdmTick = VoiceSeconds / 100 # 게이지 시간 계산.
+        for i in tqdm(range(100)):
+            time.sleep(tqdmTick)
+
+        sd.wait() # 녹화 종료 대기
+        print("녹음 끝")
+        write(fr"{sound_path}", samplerate, recording)
+    else:
+        exit()
+    # STT
+    stt_result = model.transcribe(fr"{sound_path}")
+    input_prompt = stt_result["text"]
+
+    Choose = input("해당 프롬포트가 맞습니까? \n프롬포트 : " + input_prompt + "\nTrue/False : ") #문자로 변환된 음성 출력 & 질문
+
+    if Choose == "True":
+        Choose = True
+    else:
+        Choose = False
+
+    return input_prompt, Choose
+
+While = True
+while While:
+    input_prompt, Choose = InputRecordStart()
+
+    if Choose == True:
+        last_prompt = Setting, output_base, input_prompt # 최최최종 삽입될 프롬포트
+
+        payload = { # API에게 패킷 전송
+            "model": "deepseek-r1:8b",
+            "prompt": f"{last_prompt}",
+            "stream": False
+        }
+        While = False
+
+    else:
+        time.sleep(0.5)
 
 # input_prompt = input("프롬포트 : ") # 채팅으로 프롬포트 입력
 
-last_prompt = Setting.read(), output_base, input_prompt
-Setting.close()
-
-payload = {
-    "model": "deepseek-r1:8b",
-    "prompt": f"{last_prompt}",
-    "stream": False
-}
-
+# API 죽어있을 경우 예외 처리 구문
 try:
     response = requests.post(url, json=payload)
     response.raise_for_status()
@@ -94,12 +118,15 @@ if respon is None:
 
 print("응답:", respon)
 
-f = open(After_path, "w+", encoding="utf-8")
-f.write("프롬포트 : " + input_prompt + "\n응답 : " + respon + "\n응답 시각 : " + str(datetime.now()))
-f.close()
+# 해당 질문 & 응답 등의 정보를 LOG에 저장
+with open(After_path, "w+", encoding="utf-8") as f:
+    f.write("프롬포트 : " + input_prompt + "\n응답 : " + respon + "\n응답 시각 : " + str(datetime.now()))
 
+
+# AI 응답 TTS 출력 구문
 engine.say(respon)
 engine.runAndWait()
 
+# 테스트용 AI 입력,출력 토큰 출력 구문.
 # LastTokenData = ["입력 토큰:", result.get("prompt_eval_count")],["출력 토큰:", result.get("eval_count")]
 # print(LastTokenData)
