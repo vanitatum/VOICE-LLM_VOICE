ee467371a3587b37b4df044b8c0273b080e466bb
GYU0000 <132132708+0G0Y0U0@users.noreply.github.com>
2026-02-25 22:50:46 +0900

    Update __OpenAI.py

--- VOCE_LLM/__OpenAI.py
+++ VOCE_LLM/__OpenAI.py
@@ -18,13 +18,13 @@ import time # 대기 함수용 라이브러리
 
 sd.default.device = 1 # 녹음 장치 설정
 
-InputMode = 1 # 1 : Vocie / 2 : Text # 프롬포트 입력 모드 음성, 채팅
-
-
+InputMode = 2 # 1 : Vocie / 2 : Text # 프롬포트 입력 모드 음성, 채팅
 
 url = "http://localhost:11434/api/generate"
 
 engine = pyttsx3.init() # TTs 음성 엔진
+
+AiModel = "gemma2:9b" # AI 엔진
 
 base_dir = Path(__file__).parent
 Log_path = base_dir / "Log"
@@ -36,21 +36,95 @@ AfterFileLen = BeforeFileLen + 1
 Before_path = base_dir / "Log" / f"{BeforeFileLen}.txt"
 After_path = base_dir / "Log" / f"{AfterFileLen}.txt"
 
+Arrange_Log_path = base_dir / "ArrangeLog"
+
+Arrange_Log_Token = 5 # x번째 턴마다 최근 x개의 로그를 정리해 요약.
+
+Arrange_Log_Folder_len = len(os.listdir(Arrange_Log_path)) + 1 # 요약 로그 폴더 내 파일 갯수 구하기
+
+# AI 패킷 전송 함수, return : AI 응답
+def AiApi(prompt):
+    payload = { # AI API에 패킷 전송
+        "model": AiModel,
+        "prompt": f"{prompt}",
+        "stream": False
+    }
+
+    try: # AI 대답 패킷 응답
+        response = requests.post(url, json=payload)
+        response.raise_for_status()
+
+        result = response.json()
+    
+        respon = result["response"]
+
+        return respon
+
+    except requests.RequestException as e: # API 죽었을 경우 예외 처리
+        print("API 오류:", e)
+    if respon is None:
+        exit()
+
+# 로그 정리 로그 생성 함수
+def LogArrange(i): # x : 정리할 최근 로그의 갯수. i = 5 의 경우 최근 5개를 정리 후 저장
+    if i > BeforeFileLen: # 로그 갯수가 최근 로그보다 많을 경우
+        i = BeforeFileLen # 모든 로그를 범위로 가져감
+
+    # 종합 과거 로그 설정
+    TotalBefore_Log = ""
+    x = BeforeFileLen
+
+    while i > 0:
+        Before_path = base_dir / "Log" / f"{x}.txt"
+
+        with open(Before_path, "r", encoding="utf-8") as Current_Log:
+            Current_Log = str(Current_Log.read())
+
+        x = (BeforeFileLen - i) + 1 #가져올 로그 넘버 계산
+        y = str(i)
+
+        TotalBefore_Log = f"{TotalBefore_Log}{y} : {Current_Log}\n"
+        
+        print(Current_Log)
+        print("----------")
+        i -= 1
+
+    AISTR = AiApi(f"해당 기록의 내용을 요약해줘.\n{TotalBefore_Log}") # 요약 내용
+    ArrangeLogFile_path = Arrange_Log_path  / f"{Arrange_Log_Folder_len}.txt" # 요약 로그 저장 위치
+    
+    with open(ArrangeLogFile_path, "w+", encoding="utf-8") as ArrangeLogFile:
+        ArrangeLogFile.write(f"{AISTR}")
+
+
+if BeforeFileLen % Arrange_Log_Token == 0: # 특정 턴마다 로그 요약
+    if Arrange_Log_Folder_len >= Arrange_Log_Token:
+        LogArrange(Arrange_Log_Token)
+
+if Arrange_Log_Folder_len > 1: # 요약 로그가 1개 이상 있을 경우, 가장 최근 요약된 로그 읽어냄
+    Arrange_Log = Arrange_Log_path / f"{Arrange_Log_Folder_len}.txt"
+
+    with open(Arrange_Log, "r", encoding="utf-8") as Arrange_Log:
+        Arrange_Log = Arrange_Log.read()
+
+
 if BeforeFileLen == 0: # 과거 로그 파일이 없을 경우.
     with open(After_path, "w+", encoding="utf-8") as ef:
         ef.write("과거 대화 기록이 없습니다.")
     Before_path = base_dir / "Log" / "1.txt"
 
-Setting_path = base_dir / "Setting.txt"
+Setting_path = base_dir / "Setting.txt" # 설정 파일 가져오기
 with open(Setting_path, "r", encoding="utf-8") as Setting:
     Setting = Setting.read()
 
+if Arrange_Log_Folder_len > 1: # 가장 최근 요약 로그가 존재할 시 프롬포트에 포함
+    with open(Before_path, "r", encoding="utf-8") as f:
+        output_base = f"가장 최근 대화 기록 요약 : {Arrange_Log}\nAI의 마지막 응답 (이것에 대해 먼저 언급하지 말 것.)" + str(f.read())
+else: # 요약 로그가 없을 경우 프롬포트만 전송.
+    with open(Before_path, "r", encoding="utf-8") as f:
+        output_base = "AI의 마지막 응답 (이것에 대해 먼저 언급하지 말 것.)" + str(f.read())
 
-with open(Before_path, "r", encoding="utf-8") as f:
-    output_base = "AI의 마지막 응답 (이것에 대해 먼저 언급하지 말 것.)" + str(f.read())
 
 # Record
-
 def InputRecordStart():
     VoiceSeconds = int(input("녹음 시간(초) 입력 : "))
     if VoiceSeconds >= 1:
@@ -95,13 +169,9 @@ if InputMode == 1: # 음성 녹음 프롬포트
         input_prompt, Choose = InputRecordStart()
 
         if Choose == True:
-            last_prompt = Setting, output_base, input_prompt # 최최최종 삽입될 프롬포트
+            last_prompt = f"{Setting} \n {output_base} \n {input_prompt}" # 최최최종 삽입될 프롬포트
 
-            payload = { # API에게 패킷 전송
-                "model": "gemma2:9b",
-                "prompt": f"{last_prompt}",
-                "stream": False
-            }
+            respon = AiApi(last_prompt)
             While = False
         else:
             time.sleep(0.5)
@@ -109,30 +179,13 @@ if InputMode == 1: # 음성 녹음 프롬포트
 
 if InputMode == 2:
     input_prompt = input("프롬포트 : ")
-    last_prompt = Setting, output_base, input_prompt
+    last_prompt = f"{Setting} \n {output_base} \n {input_prompt}"
+    respon = AiApi(last_prompt)
 
-    payload = { # API에게 패킷 전송
-        "model": "gemma2:9b",
-        "prompt": f"{last_prompt}",
-        "stream": False
-    }
 
-# input_prompt = input("프롬포트 : ") # 채팅으로 프롬포트 입력
 
-# API 죽어있을 경우 예외 처리 구문
 
-try:
-    response = requests.post(url, json=payload)
-    response.raise_for_status()
 
-    result = response.json()
-    
-    respon = result["response"]
-
-except requests.RequestException as e:
-    print("API 오류:", e)
-if respon is None:
-    exit()
 
 print("응답:", respon)
 
